61b9540f1721489cee2b8daa2bfccd5c0b5eea7a
c71n93 <korostinskii.r@phystech.edu>
2023-06-09 18:57:47 +0300

    #1 initialize dialect

--- lib/Eo/EoDialect.cpp
+++ lib/Eo/EoDialect.cpp
@@ -8,3 +8,14 @@ using namespace mlir;
 using namespace eo;
 
 #include "Eo/EoDialect.cpp.inc"
+
+/// Dialect initialization, the instance will be owned by the context. This is
+/// the point of registration of types and operations for the dialect.
+void EoDialect::initialize() {
+    addOperations<
+#define GET_OP_LIST
+#include "Eo/EoOps.cpp.inc"
+    >();
+}
+
+
